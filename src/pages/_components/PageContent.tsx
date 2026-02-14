f9bb1db6297a4fb340b644684aa2ece9b9daf527
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-15 00:07:35 +0100

    refactor: 404 page

---
+++ src/pages/_components/PageContent.tsx
@@ -0,0 +1,17 @@
+export function PageContent() {
+	return (
+		<main className='grid h-dvh place-content-center'>
+			<section className='flex flex-col gap-4'>
+				<h1 className='text-center font-stardom text-5xl tracking-tighter'>
+					404 - Not Found
+				</h1>
+				<a
+					className='rounded-sm bg-dark-primary px-4 py-2 text-center text-light-primary text-sm transition-opacity hover:opacity-75 dark:bg-light-primary dark:text-dark-primary'
+					href='/'
+				>
+					Go to Home Page
+				</a>
+			</section>
+		</main>
+	);
+}
