2578bdd39df67192a4370661d3cd9dedd7e22298
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-15 22:23:36 +0100

    chore: update 404 page

--- src/pages/404/_components/PageContent.tsx
+++ src/pages/404/_components/PageContent.tsx
@@ -1,12 +1,12 @@
 export function PageContent() {
 	return (
-		<main className='mx-auto grid size-full min-h-dvh max-w-7xl place-content-center'>
+		<main className='mx-auto grid size-full min-h-dvh max-w-7xl place-content-center px-6'>
 			<section className='flex flex-col gap-4'>
 				<h1 className='text-center font-stardom text-5xl tracking-tighter'>
 					404 - Not Found
 				</h1>
 				<a
-					className='rounded-xs border border-light-border bg-transparent px-4 py-2 text-center text-inherit text-sm transition-colors hover:bg-light-secondary dark:border-dark-border hover:dark:bg-dark-secondary'
+					className='rounded-sm bg-dark-primary px-4 py-2 text-center text-light-primary text-sm shadow-md transition-opacity hover:opacity-75 dark:bg-light-primary dark:text-dark-primary'
 					href='/'
 				>
 					Go to Home Page
