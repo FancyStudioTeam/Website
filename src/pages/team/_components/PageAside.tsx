49f84d4d73972e78f28d1436342ae7a949632ee8
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-18 00:21:09 +0100

    fix: icons

--- src/pages/team/_components/PageAside.tsx
+++ src/pages/team/_components/PageAside.tsx
@@ -1,19 +1,15 @@
-import { Sparkles } from 'lucide-react';
+import { SparkleIcon } from '@phosphor-icons/react';
 
 export function PageAside() {
 	return (
-		<aside className='flex items-center gap-2 rounded-sm border-l-4 bg-light-tertiary p-4 text-light-foreground text-sm shadow-md dark:bg-dark-tertiary dark:text-dark-foreground'>
-			<Sparkles className='size-5 shrink-0' />
+		<aside className='flex items-center gap-2 rounded-sm bg-light-tertiary p-4 text-light-foreground text-sm shadow-md dark:bg-dark-tertiary dark:text-dark-foreground'>
+			<SparkleIcon
+				className='size-5 shrink-0'
+				weight='fill'
+			/>
 			<span>
-				<strong>Would you like to join our team?</strong> Join our{' '}
-				<a
-					href='https://discord.gg/yWjeDA6ewJ'
-					rel='noopener'
-					target='_blank'
-				>
-					<strong>Discord server</strong>
-				</a>{' '}
-				for a chance to become part of our team!
+				Would you like to join our team? Join our Discord server for a
+				chance to become part of our team!
 			</span>
 		</aside>
 	);
