ccd55ebb82235b247dc55cc5bfd81f13395d543a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-02-18 09:11:23 +0100

    chore: use astro fonts

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,30 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+const WEIGHT_BOLD = 700;
+const WEIGHT_NORMAL = 400;
 
 export default defineConfig({
+	experimental: {
+		fonts: [
+			{
+				cssVariable: '--font-general-sans',
+				name: 'General Sans',
+				provider: fontProviders.fontshare(),
+				weights: [
+					WEIGHT_NORMAL,
+					WEIGHT_BOLD,
+				],
+			},
+			{
+				cssVariable: '--font-stardom',
+				name: 'Stardom',
+				provider: fontProviders.fontshare(),
+			},
+		],
+	},
 	integrations: [
 		react(),
 		sitemap(),
