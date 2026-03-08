03df37aec4d00608137717e2c314015d6f4d13ce
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-08 20:14:52 +0100

    feat: add navbar links

---
+++ src/lib/navbar/NavbarLinks.tsx
@@ -0,0 +1,51 @@
+import {
+	type Icon,
+	PenNibIcon,
+	UsersIcon,
+	WrenchIcon,
+} from '@phosphor-icons/react';
+import {
+	DiscordLogoIcon,
+	GithubLogoIcon,
+} from '@phosphor-icons/react/dist/ssr';
+import type { HTMLAttributeAnchorTarget } from 'react';
+
+export const NAVBAR_SOCIAL_MEDIA_LINKS: NavbarLink[] = [
+	{
+		href: 'https://discord.gg/yWjeDA6ewJ',
+		icon: DiscordLogoIcon,
+		target: '_blank',
+		text: 'Discord',
+	},
+	{
+		href: 'https://github.com/FancyStudioTeam',
+		icon: GithubLogoIcon,
+		target: '_blank',
+		text: 'GitHub',
+	},
+];
+
+export const NAVBAR_USEFUL_LINKS: NavbarLink[] = [
+	{
+		href: '/blog',
+		icon: PenNibIcon,
+		text: 'Blog',
+	},
+	{
+		href: '/projects',
+		icon: WrenchIcon,
+		text: 'Projects',
+	},
+	{
+		href: '/team',
+		icon: UsersIcon,
+		text: 'Team',
+	},
+];
+
+export interface NavbarLink {
+	href: string;
+	icon: Icon;
+	target?: HTMLAttributeAnchorTarget;
+	text: string;
+}
