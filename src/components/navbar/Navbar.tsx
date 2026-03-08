3bfbeff03c5d68cf403f098d69476886693f5cb7
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-08 20:17:31 +0100

    refactor: use `sticky`

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavbarContent } from './NavbarContent.tsx';
 
 export function Navbar() {
 	return (
-		<header className='fixed top-0 h-10 w-full border-dark-border border-b bg-dark-primary'>
+		<header className='sticky top-0 h-10 w-full border-dark-border border-b bg-dark-primary'>
 			<NavbarContent />
 		</header>
 	);
