bc83780fe68144eb0acf759827c1b8b0ccb58a8a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 17:27:09 +0100

    chore: add borders

--- src/components/navbar/middle/NavbarMiddleHand.tsx
+++ src/components/navbar/middle/NavbarMiddleHand.tsx
@@ -3,7 +3,7 @@ import { NavbarContentLogo } from '../NavbarContentLogo.tsx';
 
 export function NavbarMiddleHand({ open }: NavbarMiddleHandProps) {
 	return (
-		<section className='flex items-center justify-between md:justify-center'>
+		<section className='flex items-center justify-between border-dark-border md:justify-center md:border-x'>
 			<NavbarContentLogo className='mx-6 grid h-full place-items-center' />
 			<NavbarDrawerButton open={open} />
 		</section>
