8335ff35d434862690ee1ef8d99ec16bd66d352b
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 20:55:47 +0100

    chore: delete unused style

--- src/components/navbar/drawer/NavbarDrawerButton.tsx
+++ src/components/navbar/drawer/NavbarDrawerButton.tsx
@@ -3,7 +3,7 @@ import { ListIcon, XIcon } from '@phosphor-icons/react';
 
 export function NavbarDrawerButton({ open }: { open: boolean }) {
 	return (
-		<DisclosureButton className='flex cursor-pointer items-center justify-center gap-2 border-dark-border font-stardom text-md tracking-tighter transition-colors hover:bg-dark-secondary md:hidden'>
+		<DisclosureButton className='flex cursor-pointer items-center justify-center gap-2 font-stardom text-md tracking-tighter transition-colors hover:bg-dark-secondary md:hidden'>
 			{open ? (
 				<XIcon
 					className='size-5 shrink-0'
