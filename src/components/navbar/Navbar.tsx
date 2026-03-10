df5d1c0f6a783072b9028d5b64dbd599c22ec8b9
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 19:45:39 +0100

    refactor: navbar components

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,28 @@
-import { NavbarContent } from './NavbarContent.tsx';
+'use client';
+
+import { Disclosure } from '@headlessui/react';
+import { NavbarDrawerContent } from './drawer/NavbarDrawerContent.tsx';
+import { NavbarLeftHand } from './left/NavbarLeftHand.tsx';
+import { NavbarMiddleHand } from './middle/NavbarMiddleHand.tsx';
+import { NavbarRightHand } from './right/NavbarRightHand.tsx';
 
 export function Navbar() {
 	return (
 		<header className='sticky top-0 h-10 w-full border-dark-border border-b bg-dark-primary'>
-			<NavbarContent />
+			<Disclosure
+				as='nav'
+				className='mx-auto grid size-full max-w-7xl grid-cols-1 md:grid-cols-3'
+			>
+				{({ open }) => (
+					<>
+						<NavbarLeftHand />
+						<NavbarMiddleHand open={open} />
+						<NavbarRightHand />
+
+						<NavbarDrawerContent />
+					</>
+				)}
+			</Disclosure>
 		</header>
 	);
 }
