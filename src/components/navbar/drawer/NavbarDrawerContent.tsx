8288c2f9339e3666304fcae5cee5b21c3e0ca153
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 19:36:15 +0100

    fix: imports

--- src/components/navbar/drawer/NavbarDrawerContent.tsx
+++ src/components/navbar/drawer/NavbarDrawerContent.tsx
@@ -4,18 +4,18 @@ import {
 	NAVBAR_USEFUL_LINKS,
 } from '#/lib/navbar/NavbarLinks.tsx';
 import { NavbarDrawerHeader } from './header/NavbarDrawerHeader.tsx';
-import { NavbarDrawerContentLinks } from './links/NavbarDrawerContentLinks.tsx';
+import { NavbarDrawerLinks } from './links/NavbarDrawerLinks.tsx';
 
 export function NavbarDrawerContent() {
 	return (
 		<DisclosurePanel className='group fixed inset-0 z-50 backdrop-blur-xs data-open:animate-duration-150 data-open:animate-fade-in'>
 			<div className='flex flex-col gap-6 border-dark-border border-b bg-dark-primary p-6 group-data-open:animate-duration-150 group-data-open:animate-slide-in-top'>
 				<NavbarDrawerHeader />
-				<NavbarDrawerContentLinks
+				<NavbarDrawerLinks
 					links={NAVBAR_USEFUL_LINKS}
 					name='Useful Links'
 				/>
-				<NavbarDrawerContentLinks
+				<NavbarDrawerLinks
 					links={NAVBAR_SOCIAL_MEDIA_LINKS}
 					name='Social Media Links'
 				/>
