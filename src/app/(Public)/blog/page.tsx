bfce01acd119854245ade2de85f3fd564e0711ff
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 20:59:32 +0100

    feat: add "Work in Progress" pages

---
+++ src/app/(Public)/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function () {
+	return (
+		<main className='grid h-dvh-without-nav place-content-center'>
+			<h1 className='font-stardom text-5xl tracking-tighter'>
+				Work in Progress
+			</h1>
+		</main>
+	);
+}
