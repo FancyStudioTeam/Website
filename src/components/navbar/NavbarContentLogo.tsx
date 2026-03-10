ea2c6e315cc523808c59f53c48130a1b386f40e9
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-10 17:05:47 +0100

    refactor: allow `NavbarContentLogo` to use external props

--- src/components/navbar/NavbarContentLogo.tsx
+++ src/components/navbar/NavbarContentLogo.tsx
@@ -1,13 +1,24 @@
 import Link from 'next/link';
+import type { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export function NavbarContentLogo() {
+export function NavbarContentLogo({
+	className,
+	...props
+}: NavbarContentLogoProps) {
 	return (
 		<Link
-			aria-label='FancyStudio Page'
-			className='font-stardom text-xl tracking-tighter transition-opacity hover:opacity-75'
+			{...props}
+			aria-label='FancyStudio Home Page'
+			className={twMerge(
+				'font-stardom text-xl tracking-tighter transition-opacity hover:opacity-75',
+				className,
+			)}
 			href='/'
 		>
 			FancyStudio
 		</Link>
 	);
 }
+
+export type NavbarContentLogoProps = Omit<ComponentProps<typeof Link>, 'href'>;
