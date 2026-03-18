e488bd05225c8b5aded557e7815aeab0d8a24014
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-18 19:18:32 +0100

    feat: add theme provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './highlight.css';
 import './tailwind.css';
 
 import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
 import type { ReactNode } from 'react';
 import { Navbar } from '#/components/navbar/Navbar.tsx';
 import { GeistMonoVariable, GeneralSansVariable, StardomVariable } from '#/lib/Fonts.ts';
@@ -13,12 +14,20 @@ export const metadata: Metadata = createMetadataObject(MetadataPage.Home);
 
 export default function ({ children }: { children: ReactNode }) {
 	return (
-		<html lang='en-US'>
+		<html
+			lang='en-US'
+			suppressHydrationWarning={true}
+		>
 			<body
-				className={`${GeistMonoVariable} ${GeneralSansVariable} ${StardomVariable} size-full bg-dark-primary font-general-sans text-light-primary antialiased selection:bg-light-primary selection:text-dark-primary`}
+				className={`${GeistMonoVariable} ${GeneralSansVariable} ${StardomVariable} size-full bg-light-primary font-general-sans text-dark-primary antialiased selection:bg-dark-primary selection:text-light-primary dark:bg-dark-primary dark:text-light-primary dark:selection:bg-light-primary dark:selection:text-dark-primary`}
 			>
-				<Navbar />
-				{children}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
