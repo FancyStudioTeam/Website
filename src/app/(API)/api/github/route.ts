c6fec9725cee2b5f39d658c8c9275901a22d4d3a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-18 21:16:31 +0100

    chore: delete API

--- src/app/(API)/api/github/route.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server';
-
-export function POST(nextRequest: NextRequest) {
-	console.log(nextRequest);
-
-	return NextResponse.json({
-		message: 'Hello, world!',
-	});
-}
