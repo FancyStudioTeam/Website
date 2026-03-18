a21e37274456e14c4f0733543f3a7f57222d684a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-18 19:00:29 +0100

    feat: add new metadata for upcoming pages

--- src/lib/Metadata.ts
+++ src/lib/Metadata.ts
@@ -1,15 +1,20 @@
 export enum MetadataPage {
 	Home = 'HOME',
+	OurProjects = 'OUR_PROJECTS',
+	OurTeam = 'OUR_TEAM',
 	NotFound = 'NOT_FOUND',
 }
 
 export const METADATA_PAGE_TITLE: Record<MetadataPage, string> = {
 	[MetadataPage.Home]: 'Home',
+	[MetadataPage.OurProjects]: 'Our Projects',
+	[MetadataPage.OurTeam]: 'Our Team',
 	[MetadataPage.NotFound]: 'Not Found',
 };
 
 export const METADATA_PAGE_DESCRIPTION: Record<MetadataPage, string> = {
-	[MetadataPage.Home]:
-		'Welcome to FancyStudio! FancyStudio is a team of people who love to create and develop projects for everyone to use.',
-	[MetadataPage.NotFound]: 'This page has not been found or is currently unavailable.',
+	[MetadataPage.Home]: 'Welcome to the official FancyStudio website.',
+	[MetadataPage.OurProjects]: 'Discover the projects we have created or contributed to.',
+	[MetadataPage.OurTeam]: 'Meet the people who make up our FancyStudio team.',
+	[MetadataPage.NotFound]: 'This page was not found or is currently unavailable.',
 };
